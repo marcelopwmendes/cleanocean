c7d2a6cfc16515781cb1d5a11c44dd36ec6aa5d7
CMScheid <carlamontibeller@gmail.com>
2019-06-13 13:45:40 +0100

    include sound class, and cleaning not useful stuff in other classes

--- src/org/academiadecodigo/murlogs/cleanocean/gameobjects/EcoFactory.java
+++ src/org/academiadecodigo/murlogs/cleanocean/gameobjects/EcoFactory.java
@@ -1,7 +1,6 @@
 package org.academiadecodigo.murlogs.cleanocean.gameobjects;
 
 import org.academiadecodigo.murlogs.cleanocean.Main;
-import org.academiadecodigo.murlogs.cleanocean.gameobjects.trash.Trash;
 import org.academiadecodigo.murlogs.cleanocean.gameobjects.trash.TrashType;
 import org.academiadecodigo.murlogs.cleanocean.grid.Grid;
 
@@ -10,7 +9,6 @@ public class EcoFactory {
     private static int col = Main.COLS;
     private static int row = 0;
     private static int i = -1;
-
     private static String[] eco = {"ecoPaper40.png", "ecoMetal40.png",  "ecoPlastic40.png", "ecoGlass40.png", "ecoOrganic40.png"};
 
 
